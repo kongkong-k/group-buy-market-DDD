3968d224a5f9df87b3bd5009e87d7f3918a3c2ec
zhangsihan <1835638931@qq.com>
2026-04-12 11:28:05 +0800

    test success

---
+++ group-buy-market-DDD-app/src/test/java/com/qcby/test/infrastructure/dao/GroupBuyDiscountDaoTest.java
@@ -0,0 +1,29 @@
+package com.qcby.test.infrastructure.dao;
+
+import com.alibaba.fastjson.JSON;
+import com.qcby.infrastructure.dao.IGroupBuyDiscountDao;
+import com.qcby.infrastructure.dao.po.GroupBuyDiscount;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupBuyDiscountDaoTest {
+
+    @Resource
+    private IGroupBuyDiscountDao groupBuyDiscountDao;
+
+    @Test
+    public void test_queryGroupBuyDiscountList(){
+        List<GroupBuyDiscount> groupBuyDiscounts = groupBuyDiscountDao.queryGroupBuyDiscountList();
+        log.info("测试结果:{}", JSON.toJSONString(groupBuyDiscounts));
+    }
+
+}
